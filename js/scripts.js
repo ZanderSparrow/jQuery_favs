03a15fcf8ce079256dac01c139f7d88012214f96
Alex Silverhawk <sondra.silverhawk@hush.com>
2015-10-09 14:21:18 -0700

    write h2 text in output, 'Your favorites:' if name field empty, or 'So and So's favorites:' if name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(function() {
 	var favorites = [];
+	var userName = '';
 	$('button#add').click(function () {
 		var newFavorite = $('input#appendInputButton').val();
-		favorites.push( newFavorite );	
+		favorites.push( newFavorite );
 	});
 	$('#submit').click(function(event) {
 		event.preventDefault();
+		userName = $('#user-name').val();
+		//Get name from name field. If blank, say 'Your' favorites: otherwise 'Name' s favorites:
+		if(userName === '') {
+			$('#output h2').text("Your favorites:");
+		} else {
+			$('#output h2').text( userName + "'s favorites:" );
+		}
+
+		//use favorites.map and a callback to:
+			//1. wrap each item in an li tag
+			//2. append the item to the #output ul 
+		$('#output').show();
 	});
 });
