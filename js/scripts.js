9e1ad8e18c07e5185865ade01bdeeb62eb8431ea
Alex Silverhawk <sondra.silverhawk@hush.com>
2015-10-09 12:46:42 -0700

    add event listeners for buttons and create an array to store favorites

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(function() {
-	
+	var favorites = [];
+	$('button#add').click(function () {
+		var newFavorite = $('input#appendInputButton').val();
+		favorites.push( newFavorite );	
+	});
+	$('#submit').click(function(event) {
+		event.preventDefault();
+	});
 });
